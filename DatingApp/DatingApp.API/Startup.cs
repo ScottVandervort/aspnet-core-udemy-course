ff359e06b5827e3e66dab17e0090ad5a913f1a59
Scott Vandervort <svandervort@gmail.com>
2020-09-29 09:14:15 -0700

    Finished Module 2 / Section 22: Added Angular and Bootstrap.
    Angular makes simple request against API to return and display data.

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             // The connection string is pulled from appsetting.json.
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,9 @@ namespace DatingApp.API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // For now, allow everything...this is returned in the header of the HTTP response.
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
             app.UseAuthorization();
 
